2e6234b9846b1935d9614430a4e0a952a56beaf4
anooanand <anoo.anand@gmail.com>
2025-08-03 21:01:14 +1000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react( )],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/auth/v1': {
+        target: 'https://rvlotczavccreigdzczo.supabase.co',
+        changeOrigin: true,
+        secure: true
+      },
+      '/rest/v1': {
+        target: 'https://rvlotczavccreigdzczo.supabase.co',
+        changeOrigin: true,
+        secure: true
+      }
+    }
   }
 });
