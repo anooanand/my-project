18a662bb43ba947d9b804cb880d7d82fac11ed51
anooanand <anoo.anand@gmail.com>
2025-08-30 10:36:32 +1000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './darkmode.css';
+import './styles/kid-theme.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { LearningProvider } from './contexts/LearningContext';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <LearningProvider>
+        <App />
+      </LearningProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
