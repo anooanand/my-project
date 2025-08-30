76860313d52c156ebf5fcf5e6789b74b0cfac971
anooanand <anoo.anand@gmail.com>
2025-08-30 10:35:19 +1000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins: [react( )],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443
     },
-  },
-})
-
+    proxy: {
+      '/auth/v1': {
+        target: 'https://rvlotczavccreigdzczo.supabase.co',
+        changeOrigin: true,
+        secure: true
+      },
+      '/rest/v1': {
+        target: 'https://rvlotczavccreigdzczo.supabase.co',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
+});
