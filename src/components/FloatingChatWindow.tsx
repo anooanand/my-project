0e84b1261fc07690ea731035bdfb56144371ad47
anooanand <anoo.anand@gmail.com>
2025-09-18 07:42:05 +1000

    Updated FloatingChatWindow.tsx

--- src/components/FloatingChatWindow.tsx
+++ src/components/FloatingChatWindow.tsx
@@ -61,7 +61,7 @@ export function FloatingChatWindow({
   return (
     <div
       ref={chatRef}
-      className={`attached-chat-container fixed right-0 top-16 bottom-0 bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700 shadow-xl transition-all duration-300 z-40 ${
+      className={`attached-chat-container fixed right-0 bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700 shadow-xl transition-all duration-300 z-40 ${
         isMinimized ? 'w-12' : isCollapsed ? 'w-16' : 'w-96'
       }`}
     >
