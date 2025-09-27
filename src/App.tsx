5a3c22201568430740559f8cb85ed938ed5e486a
anooanand <anoo.anand@gmail.com>
2025-09-27 19:54:56 +1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
+/**
+ * FIXED App.tsx - Remove SafeModeWrapper to prevent safe mode activation
+ * Replace your src/App.tsx with this content
+ */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { AppProvider } from './contexts/AppContext';
 import AppContent from './components/AppContent';
-import SafeModeWrapper, { setupGlobalErrorHandling } from './components/SafeModeWrapper';
-
-// Initialize global error handling
-setupGlobalErrorHandling();
 
 function App() {
-  // Set up safe mode event listener
-  React.useEffect(() => {
-    const handleSafeModeEvent = (event: CustomEvent) => {
-      console.warn('Safe mode requested:', event.detail);
-      // The SafeModeWrapper will handle this automatically
-    };
-
-    window.addEventListener('enableSafeMode', handleSafeModeEvent as EventListener);
-    
-    return () => {
-      window.removeEventListener('enableSafeMode', handleSafeModeEvent as EventListener);
-    };
-  }, []);
-
   return (
-    <SafeModeWrapper>
-      <ThemeProvider>
-        <AuthProvider>
-          <AppProvider>
-            <Router>
-              <AppContent />
-            </Router>
-          </AppProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </SafeModeWrapper>
+    <ThemeProvider>
+      <AuthProvider>
+        <AppProvider>
+          <Router>
+            <AppContent />
+          </Router>
+        </AppProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
