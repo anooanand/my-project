cd428cbbd5496cd146a9f611a756a1e3ce974708
anooanand <anoo.anand@gmail.com>
2025-09-27 21:53:31 +1000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-/**
- * FIXED App.tsx - Remove SafeModeWrapper to prevent safe mode activation
- * Replace your src/App.tsx with this content
- */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { AppProvider } from './contexts/AppContext';
 import AppContent from './components/AppContent';
+import NSWDemoPage from './components/NSWDemoPage';
 
 function App() {
   return (
