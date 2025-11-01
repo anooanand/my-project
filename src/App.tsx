db7896e1abd4b72fa054540b0dcbb8c3cc934703
anooanand <anoo.anand@gmail.com>
2025-11-01 21:10:56 +1100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { AppProvider } from './contexts/AppContext';
 import { LearningProvider } from './contexts/LearningContext';
 import AppContent from './components/AppContent';
 import NSWDemoPage from './components/NSWDemoPage';
+
+// Component to handle URL parameters
+const ReferralHandler: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const refCode = params.get('ref');
+
+    if (refCode) {
+      // Store the referral code in local storage
+      localStorage.setItem('referred_by', refCode);
+      console.log('Referral code stored:', refCode);
+      
+      // Optionally, clean the URL to prevent sharing the ref parameter
+      // This is commented out as it might interfere with other routing logic
+      // if (window.history.replaceState) {
+      //   const url = new URL(window.location.href);
+      //   url.searchParams.delete('ref');
+      //   window.history.replaceState({path: url.href}, '', url.href);
+      // }
+    }
+  }, [location.search]);
+
+  return <>{children}</>;
+};
 
 function App() {
   return (
@@ -14,7 +40,9 @@ function App() {
         <AppProvider>
           <LearningProvider>
             <Router>
-              <AppContent />
+              <ReferralHandler>
+                <AppContent />
+              </ReferralHandler>
             </Router>
           </LearningProvider>
         </AppProvider>
